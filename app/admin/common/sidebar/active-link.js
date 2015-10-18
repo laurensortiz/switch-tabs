8a221091ddc4786eda9673d8497b0b08b01bc0b3
Laurens OB <laurensortiz@laurens.local>
2015-10-18 04:30:04 -0600

    menu active item directive

--- app/admin/common/sidebar/active-link.js
+++ app/admin/common/sidebar/active-link.js
@@ -5,10 +5,34 @@ angular.module('switchTabsAppAdmin')
 
   return {
     restrict : 'A',
+    link : function ( scope, element, attrs, controller ){
+      var activeClass = 'active',
+          path = (attrs.href || attr[0].attributes['data-href'].value);
 
-  }
+      //Verify in case the path is / because that means should be /admin/
+      if ( path === '/admin/' ) {
+        path = '/';
+      }
+      else {
+        //Is necessary include a '/' before the path name because
+        // location.path() returns the name with a '/' at the beginning
+        path = '/' + path;
+      }
 
+      scope.location = $location;
+      
+      scope.$watch('location.path()', function (newPath) {
+        if ( newPath === path ) {
+          console.log('yes');
+          element.addClass(activeClass);
+        }
+        else {
+          element.removeClass(activeClass);
+        }
 
+      });
 
+    }
 
+  };
   }]);
